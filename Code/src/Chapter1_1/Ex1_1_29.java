7ea17e77e0ba25ddd802fd9b20f23a3c99efe4fe
graham <1731354527@qq.com>
2020-10-09 10:08:21 +0800

    Chapter1 Exercise

--- Code/src/Chapter1_1/Ex1_1_29.java
+++ Code/src/Chapter1_1/Ex1_1_29.java
@@ -1,5 +1,7 @@
 package Chapter1_1;
 
+import java.util.Arrays;
+
 public class Ex1_1_29 {
     //返回有序数组a中，小于key的元素个数
     public static int rank(int key, int[] a)
@@ -13,26 +15,39 @@ public class Ex1_1_29 {
                 high = mid - 1;
             else if(key > a[mid])
                 low = mid + 1;
+            else if(mid > 0 && a[mid-1] == key) //在重复元素中，保证选择index最小的元素
+                high = mid - 1;
             else
-            {
-                while (a[mid] == a[mid-1] && mid > 0)
-                    mid -= 1;
-            }
-            return mid;
+                return mid;
         }
         return -1;
     }
 
-       public static int count (int key, int[] a)
+   public static int count (int key, int[] a)
+   {
+       int cnt = 1;
+       //找到最小的i
+       int index = rank(key,a);
+
+       //没有找到key
+       if(index == a.length || a[index] != key)
+           return -1;
+
+       while(a[index] == a[index+1] && index < a.length-1)
        {
-           int cnt = 0;
-           //找到最小的i
-           int i = rank(key,a);
-           while(a[i]==a[i+1] && i<a.length)
-           {
-               cnt++;
-               i++;
-           }
-           return cnt;
+           cnt++;
+           index++;
        }
+       return cnt;
+   }
+
+    public static void main(String[] args) {
+        int[] whitelist = {1,3,5,5,5,6,6,6,6,7,8};
+        Arrays.sort(whitelist);
+        int key = 6;
+        int index = rank(key, whitelist);
+        System.out.println("共有" + index + "个元素比" + key + "小");
+        int count = count(key, whitelist);
+        System.out.println("共有" + count + "个元素跟" + key + "一样大");
+    }
 }
