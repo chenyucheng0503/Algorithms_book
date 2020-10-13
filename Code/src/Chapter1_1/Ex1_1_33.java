fbd7458a10676c6e409436c93318e902ec0f178e
graham <1731354527@qq.com>
2020-10-13 16:26:49 +0800

    Chapter1 Exercise

--- Code/src/Chapter1_1/Ex1_1_33.java
+++ Code/src/Chapter1_1/Ex1_1_33.java
@@ -3,8 +3,12 @@ package Chapter1_1;
 public class Ex1_1_33 {
 }
 
-class Matrix{
-    static double dot(double[] x, double[] y){
+class Matrix {
+    static double dot(double[] x, double[] y) {
         return 0;
+    }
+
+    static double[][] mult(double[][] a, double[][] b) {
+        return new double[1][1];
     }
 }
